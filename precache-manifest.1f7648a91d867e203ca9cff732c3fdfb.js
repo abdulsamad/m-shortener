dff91a50b39e5d89cd2d240712cae8beff06ca1b
Abdul Samad <Samk8778@yahoo.com>
2020-03-28 02:20:54 +0530

    Updates

---
+++ precache-manifest.1f7648a91d867e203ca9cff732c3fdfb.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ac655bd58a56b438bf18d7bd8c351ef1",
+    "url": "/index.html"
+  },
+  {
+    "revision": "9b852f62e7e1abd2a853",
+    "url": "/static/css/main.3307f635.chunk.css"
+  },
+  {
+    "revision": "c8be61c93431106f6959",
+    "url": "/static/js/2.072d6ad2.chunk.js"
+  },
+  {
+    "revision": "b3924902b6517db7f2b56facf8ba237b",
+    "url": "/static/js/2.072d6ad2.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9b852f62e7e1abd2a853",
+    "url": "/static/js/main.c9dfcada.chunk.js"
+  },
+  {
+    "revision": "6629d5e870f13df34b3d",
+    "url": "/static/js/runtime-main.be8148e3.js"
+  }
+]);
