9b122ad0407070407ef78b10c42260eb85caa66d
Abdul Samad <Samk8778@yahoo.com>
2020-05-19 21:17:12 +0530

    :sparkles: Added search

--- src/components/List.js
+++ src/components/List.js
@@ -1,33 +1,23 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import {
-	Button,
-	Collection,
-	CollectionItem,
-	Col,
-	Dropdown,
-	Icon,
-	Modal,
-	Pagination,
-	Row,
-} from 'react-materialize';
-import M from 'materialize-css';
+import { Collection, CollectionItem, Col, Icon, Pagination, Row } from 'react-materialize';
 import Copy from './Copy';
+import ListHead from './ListHead';
 
 function List({ match }) {
 	const history = useHistory();
-	const linksCollection = localStorage.getItem('linksCollection');
-	const linkHash = 'e5a9cc5a85b282aec3acbc5f95bd009a';
 	const [urlList, setUrlList] = useState([]);
 	const [totalPages, setTotalPages] = useState(0);
 	const [activePage, setActivePage] = useState(1);
+	const [showSearch, setShowSearch] = useState(false);
 
 	useEffect(() => {
+		const linksCollection = localStorage.getItem('linksCollection');
+
 		if (linksCollection) {
 			let num = 1;
 			const step = 10;
 			const length = JSON.parse(linksCollection).length;
-
 			while (step * num < length) {
 				num++;
 			}
@@ -40,158 +30,45 @@ function List({ match }) {
 
 			setUrlList(JSON.parse(linksCollection).slice(activePage * step - step, activePage * step));
 		}
-	}, [totalPages, activePage, match, linksCollection]);
+	}, [totalPages, activePage, match]);
 
-	const exportData = () => {
-		const link = document.createElement('a');
-		const data = JSON.parse(localStorage.getItem('linksCollection'));
+	const onSearch = (ev, step = 10) => {
+		const filteredUrlList = JSON.parse(localStorage.getItem('linksCollection')).filter((url) => {
+			const regex = new RegExp(ev.target.value, 'gi');
 
-		if (data) {
-			data.push({ id: linkHash });
-
-			link.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
-			link.download = document.domain + '.json';
-			document.body.appendChild(link);
-			link.click();
-			document.body.removeChild(link);
-
-			M.toast({
-				html: `<i class='material-icons blue-text'>info</i> &nbsp; Please download your backup and Keep it safe.`,
-			});
-		} else {
-			M.toast({
-				html: `<i class='material-icons red-text'>error</i> &nbsp; Nothing to export`,
-				classes: 'error-toast',
-			});
-		}
-	};
-
-	const importData = (ev) => {
-		const file = ev.target.files[0];
-
-		if (file) {
-			try {
-				const reader = new FileReader();
-
-				if (file.type === 'application/json') {
-					reader.readAsText(file, 'UTF-8');
-
-					reader.onload = function (ev) {
-						const data = JSON.parse(ev.target.result);
-						const fileId = data.pop().id;
-						const linksCollection = localStorage.getItem('linksCollection');
-
-						if (fileId === linkHash) {
-							if (linksCollection === null) {
-								localStorage.setItem('linksCollection', JSON.stringify(data));
-							} else {
-								const concatData = data.concat(JSON.parse(linksCollection));
-								localStorage.setItem('linksCollection', JSON.stringify(concatData));
-							}
-
-							window.location.reload();
-						} else {
-							throw new Error('Not a valid file');
-						}
-					};
-				} else {
-					throw new Error('Not a valid file');
-				}
-			} catch (err) {
-				M.toast({
-					html: `<i class='material-icons red-text'>error</i> &nbsp; ${err.message}`,
-					classes: 'error-toast',
-				});
+			if (!url.title) {
+				return url.url.match(regex) || url.shorturl.match(regex);
 			}
-		}
+			return url.url.match(regex) || url.shorturl.match(regex) || url.title.match(regex);
+		});
+
+		setUrlList(filteredUrlList);
 	};
 
-	const deleteData = () => {
-		localStorage.removeItem('linksCollection');
-		window.location.reload();
+	const onSearchCancel = () => {
+		setShowSearch(false);
+		history.push('/');
 	};
 
 	return (
 		<section className='links-collection z-depth-2 hoverable'>
 			<Collection
 				header={
-					<div className='list-head'>
-						<div className='heading'>Recent Shorten Links</div>
-						<span className='list-dropdown'>
-							<Dropdown
-								id='list-dropdown'
-								options={{
-									alignment: 'left',
-									autoTrigger: true,
-									closeOnClick: true,
-									constrainWidth: true,
-									container: null,
-									coverTrigger: true,
-									hover: false,
-									inDuration: 150,
-									onCloseEnd: null,
-									onCloseStart: null,
-									onOpenEnd: null,
-									onOpenStart: null,
-									outDuration: 250,
-								}}
-								trigger={
-									<a href='#!'>
-										<Icon right>more_vert</Icon>
-									</a>
-								}>
-								<a href='#!' onChange={importData}>
-									<Icon left>import_export</Icon>
-									<input type='file' className='import-file-input' />
-								</a>
-								<a href='#!' onClick={exportData}>
-									<Icon left>cloud_download</Icon> Export
-								</a>
-								<a className='modal-trigger' href='#delete-modal' node='a'>
-									<Icon left>delete</Icon> Delete
-								</a>
-							</Dropdown>
-						</span>
-						<Modal
-							bottomSheet={false}
-							fixedFooter={false}
-							header='Delete All links stored locally?'
-							id='delete-modal'
-							open={false}
-							options={{
-								dismissible: true,
-								endingTop: '10%',
-								inDuration: 250,
-								onCloseEnd: null,
-								onCloseStart: null,
-								onOpenEnd: null,
-								onOpenStart: null,
-								opacity: 0.5,
-								outDuration: 250,
-								preventScrolling: true,
-								startingTop: '4%',
-							}}>
-							<h6>All yours created short links will continue to redirect to their destination.</h6>
-							<p>
-								You can backup you data&nbsp;
-								<a href='#!' onClick={exportData}>
-									here.
-								</a>
-							</p>
-							<Button className='red darken-1' node='button' onClick={deleteData}>
-								Yes
-							</Button>
-						</Modal>
-					</div>
+					<ListHead
+						onSearch={onSearch}
+						onSearchCancel={onSearchCancel}
+						showSearch={showSearch}
+						setShowSearch={setShowSearch}
+					/>
 				}>
-				{urlList.length === 0 && activePage <= totalPages && (
+				{urlList.length === 0 && (
 					<CollectionItem>
 						<br />
 						<h5 className='grey-text'>Your history will appear here.</h5>
 						<br />
 					</CollectionItem>
 				)}
-				{activePage > totalPages && (
+				{activePage > totalPages && urlList.length > 0 && (
 					<CollectionItem>
 						<br />
 						<h5 className='grey-text'>Page not found</h5>
@@ -226,18 +103,20 @@ function List({ match }) {
 						</Row>
 					</CollectionItem>
 				))}
-				<CollectionItem className='center-align'>
-					<Pagination
-						activePage={activePage}
-						items={totalPages}
-						leftBtn={<Icon>chevron_left</Icon>}
-						maxButtons={5}
-						rightBtn={<Icon>chevron_right</Icon>}
-						onSelect={(num) => {
-							history.push(`/${num}`);
-						}}
-					/>
-				</CollectionItem>
+				{!showSearch && (
+					<CollectionItem className='center-align'>
+						<Pagination
+							activePage={activePage}
+							items={totalPages}
+							leftBtn={<Icon>chevron_left</Icon>}
+							maxButtons={5}
+							rightBtn={<Icon>chevron_right</Icon>}
+							onSelect={(num) => {
+								history.push(`/${num}`);
+							}}
+						/>
+					</CollectionItem>
+				)}
 			</Collection>
 		</section>
 	);
---
+++ src/components/ListHead.js
@@ -0,0 +1,163 @@
+import React from 'react';
+import M from 'materialize-css';
+import { Dropdown, Modal, Icon, Button, TextInput } from 'react-materialize';
+
+function ListHead({ onSearch, onSearchCancel, showSearch, setShowSearch }) {
+	const linkHash = 'e5a9cc5a85b282aec3acbc5f95bd009a';
+
+	const exportData = () => {
+		const link = document.createElement('a');
+		const data = JSON.parse(localStorage.getItem('linksCollection'));
+
+		if (data) {
+			data.push({ id: linkHash });
+
+			link.href = 'data:text/json;charset=utf-8,' + encodeURIComponent(JSON.stringify(data));
+			link.download = document.domain + '.json';
+			document.body.appendChild(link);
+			link.click();
+			document.body.removeChild(link);
+
+			M.toast({
+				html: `<i class='material-icons blue-text'>info</i> &nbsp; Please download your backup and Keep it safe.`,
+			});
+		} else {
+			M.toast({
+				html: `<i class='material-icons red-text'>error</i> &nbsp; Nothing to export`,
+				classes: 'error-toast',
+			});
+		}
+	};
+
+	const importData = (ev) => {
+		const file = ev.target.files[0];
+
+		if (file) {
+			try {
+				const reader = new FileReader();
+
+				if (file.type === 'application/json') {
+					reader.readAsText(file, 'UTF-8');
+
+					reader.onload = function (ev) {
+						const data = JSON.parse(ev.target.result);
+						const fileId = data.pop().id;
+						const linksCollection = localStorage.getItem('linksCollection');
+
+						if (fileId === linkHash) {
+							if (linksCollection === null) {
+								localStorage.setItem('linksCollection', JSON.stringify(data));
+							} else {
+								const concatData = data.concat(JSON.parse(linksCollection));
+								localStorage.setItem('linksCollection', JSON.stringify(concatData));
+							}
+
+							window.location.reload();
+						} else {
+							throw new Error('Not a valid file');
+						}
+					};
+				} else {
+					throw new Error('Not a valid file');
+				}
+			} catch (err) {
+				M.toast({
+					html: `<i class='material-icons red-text'>error</i> &nbsp; ${err.message}`,
+					classes: 'error-toast',
+				});
+			}
+		}
+	};
+
+	const deleteData = () => {
+		localStorage.removeItem('linksCollection');
+		window.location.reload();
+	};
+
+	if (showSearch) {
+		return (
+			<div className='search-input-container'>
+				<TextInput placeholder='Enter Search Keyword' onKeyUp={onSearch} autoFocus />
+				<Button className='red darken-1' onClick={onSearchCancel}>
+					<Icon>close</Icon>
+				</Button>
+			</div>
+		);
+	}
+
+	return (
+		<div className='list-head'>
+			<div className='heading'>Recent Shorten Links</div>
+			<span className='list-dropdown'>
+				<a href='#!' onClick={() => setShowSearch(true)}>
+					<Icon>search</Icon>
+				</a>
+				<Dropdown
+					id='list-dropdown'
+					options={{
+						alignment: 'left',
+						autoTrigger: true,
+						closeOnClick: true,
+						constrainWidth: true,
+						container: null,
+						coverTrigger: true,
+						hover: false,
+						inDuration: 150,
+						onCloseEnd: null,
+						onCloseStart: null,
+						onOpenEnd: null,
+						onOpenStart: null,
+						outDuration: 250,
+					}}
+					trigger={
+						<a href='#!'>
+							<Icon right>more_vert</Icon>
+						</a>
+					}>
+					<a href='#!' onChange={importData}>
+						<Icon left>import_export</Icon>
+						<input type='file' className='import-file-input' />
+					</a>
+					<a href='#!' onClick={exportData}>
+						<Icon left>cloud_download</Icon> Export
+					</a>
+					<a className='modal-trigger' href='#delete-modal' node='a'>
+						<Icon left>delete</Icon> Delete
+					</a>
+				</Dropdown>
+			</span>
+			<Modal
+				bottomSheet={false}
+				fixedFooter={false}
+				header='Delete All links stored locally?'
+				id='delete-modal'
+				open={false}
+				options={{
+					dismissible: true,
+					endingTop: '10%',
+					inDuration: 250,
+					onCloseEnd: null,
+					onCloseStart: null,
+					onOpenEnd: null,
+					onOpenStart: null,
+					opacity: 0.5,
+					outDuration: 250,
+					preventScrolling: true,
+					startingTop: '4%',
+				}}>
+				<h6>All yours created short links will continue to redirect to their destination.</h6>
+				<p>
+					You can backup you data&nbsp;
+					<a href='#!' onClick={exportData}>
+						here.
+					</a>
+				</p>
+				<Button className='red darken-1' node='button' onClick={deleteData}>
+					Yes
+				</Button>
+			</Modal>
+		</div>
+	);
+}
+
+export default ListHead;
