d05ecaabc0080dd9f16844bed1935cf76285418f
Abdul Samad <Samk8778@yahoo.com>
2020-06-12 23:47:40 +0530

    :sparkles: Added edit URL option and :bug: Fixed delete bug

--- src/components/list/List.js
+++ src/components/list/List.js
@@ -1,6 +1,16 @@
 import React, { useState, useEffect } from 'react';
 import { useHistory } from 'react-router-dom';
-import { Collection, CollectionItem, Col, Icon, Pagination, Row } from 'react-materialize';
+import {
+	Collection,
+	CollectionItem,
+	Col,
+	Icon,
+	Pagination,
+	Row,
+	Modal,
+	Button,
+	TextInput,
+} from 'react-materialize';
 import Copy from '../Copy';
 import ListHead from './ListHead';
 
@@ -10,7 +20,13 @@ function List({ match }) {
 	const [totalPages, setTotalPages] = useState(0);
 	const [activePage, setActivePage] = useState(1);
 	const [showSearch, setShowSearch] = useState(false);
-	const [editModeActive, setEditModeActive] = useState(true);
+	const [editModeActive, setEditModeActive] = useState(false);
+	const [editObj, setEditObj] = useState({
+		url: '',
+		shorturl: '',
+		title: '',
+		id: '',
+	});
 
 	useEffect(() => {
 		const linksCollection = localStorage.getItem('linksCollection');
@@ -54,16 +70,22 @@ function List({ match }) {
 		history.push('/');
 	};
 
-	const deleteURL = (ev) => {
-		const id = ev.target.parentElement.parentElement.parentElement
-			.querySelector('.shorturl')
-			.innerText.replace('https://is.gd/', '');
+	const deleteURL = (id) => {
 		const linksCollection = JSON.parse(localStorage.getItem('linksCollection'));
-		const item = linksCollection.filter((url) => url.id !== id);
+		const newLinksCollection = linksCollection.filter((url) => url.id !== id);
+		const newUrlList = urlList.filter((url) => url.id !== id);
 
-		localStorage.setItem('linksCollection', JSON.stringify(item));
-		ev.target.closest('.collection-item').remove();
-		console.log(linksCollection.length);
+		setUrlList(newUrlList);
+		localStorage.setItem('linksCollection', JSON.stringify(newLinksCollection));
+	};
+
+	const editURL = ({ id }) => {
+		const linksCollection = JSON.parse(localStorage.getItem('linksCollection'));
+		const newLinksCollection = linksCollection.map((url) => (url.id === id ? editObj : url));
+		const newUrlList = urlList.map((url) => (url.id === id ? editObj : url));
+
+		setUrlList(newUrlList);
+		localStorage.setItem('linksCollection', JSON.stringify(newLinksCollection));
 	};
 
 	return (
@@ -93,13 +115,34 @@ function List({ match }) {
 					</CollectionItem>
 				)}
 				{urlList.map((link, index) => (
-					<CollectionItem key={index}>
+					<CollectionItem
+						key={index}
+						onClick={(ev) => {
+							const linksCollection = JSON.parse(localStorage.getItem('linksCollection'));
+							const id = ev.target.parentElement.parentElement.parentElement
+								.querySelector('.shorturl')
+								.innerText.replace('https://is.gd/', '');
+
+							if (ev.target.classList.contains('edit')) {
+								let obj = linksCollection.find((url) => url.id === id);
+								setEditObj({ title: '', ...obj });
+							} else if (ev.target.classList.contains('delete')) {
+								deleteURL(id);
+							}
+						}}>
 						<Row>
 							<Col s={10}>
 								{editModeActive && (
-									<div>
-										<a href='#!' onClick={deleteURL}>
-											<Icon left>delete</Icon>
+									<div className='edit-mode-icons'>
+										<a href='#Edit-Modal' className='modal-trigger'>
+											<Icon left className='edit'>
+												edit
+											</Icon>
+										</a>
+										<a href='#!'>
+											<Icon left className='delete'>
+												delete
+											</Icon>
 										</a>
 									</div>
 								)}
@@ -146,6 +189,61 @@ function List({ match }) {
 					</CollectionItem>
 				)}
 			</Collection>
+			<Modal
+				actions={[
+					<Button modal='close' node='button' className='red'>
+						Cancel
+					</Button>,
+					<span>&nbsp;&nbsp;&nbsp;</span>,
+					<Button modal='close' node='button' className='blue' onClick={() => editURL(editObj)}>
+						Edit
+					</Button>,
+				]}
+				className='edit-modal'
+				bottomSheet={false}
+				fixedFooter
+				header='Edit URL'
+				id='Edit-Modal'
+				options={{
+					dismissible: true,
+					endingTop: '10%',
+					inDuration: 250,
+					onCloseEnd: null,
+					onCloseStart: null,
+					onOpenEnd: null,
+					onOpenStart: null,
+					opacity: 0.5,
+					outDuration: 250,
+					preventScrolling: true,
+					startingTop: '4%',
+				}}>
+				<div style={{ paddingTop: '10px' }}>
+					<TextInput
+						type='text'
+						icon='title'
+						value={editObj.title}
+						name='title'
+						onChange={(ev) => setEditObj({ ...editObj, [ev.target.name]: ev.target.value })}
+						label='Title'
+					/>
+					<TextInput
+						type='url'
+						icon='public'
+						value={editObj.url}
+						name='url'
+						label='Full URL'
+						readOnly
+					/>
+					<TextInput
+						type='url'
+						icon='link'
+						value={editObj.shorturl}
+						name='shorturl'
+						label='Short URL'
+						readOnly
+					/>
+				</div>
+			</Modal>
 		</section>
 	);
 }
