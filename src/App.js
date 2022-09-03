666c317544fd137e60076d7f9fca0c72b00789b4
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-03 11:45:41 +0530

    :construction: Removed react-router basename

--- src/App.js
+++ src/App.js
@@ -1,24 +1,23 @@
-import React, { useEffect } from "react";
-import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
-import localForage from "localforage";
-import Navbar from "./components/layout/Navbar";
-import Form from "./components/layout/Form";
-import List from "./components/layout/list/List";
-import Footer from "./components/layout/Footer";
-import "./scss/App.scss";
+import React, { useEffect } from 'react';
+import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
+import localForage from 'localforage';
+import Navbar from './components/layout/Navbar';
+import Form from './components/layout/Form';
+import List from './components/layout/list/List';
+import Footer from './components/layout/Footer';
+import './scss/App.scss';
 
 function App() {
   useEffect(() => {
-    if (localStorage.getItem("theme"))
-      document.body.className = localStorage.getItem("theme");
+    if (localStorage.getItem('theme')) document.body.className = localStorage.getItem('theme');
 
     localForage.config({
-      name: "URL Shortener",
+      name: 'URL Shortener',
     });
   }, []);
 
   return (
-    <BrowserRouter basename="/url_shortener_is.gd">
+    <BrowserRouter>
       <div className="App">
         <Navbar title="URL Shortener" />
         <div className="container">
