3f95876f08180a3b8a998cbbaa8e921f4e074c61
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-03 11:25:18 +0530

    Updates

--- precache-manifest.bbb1a62b6221bed52dbcf68859a56d20.js
+++ precache-manifest.bbb1a62b6221bed52dbcf68859a56d20.js
@@ -1,11 +1,11 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "24c6ad6fe53b111ad3040130c00e41ed",
+    "revision": "ffb07a1b96c45b84d56691fdb4cd83ea",
     "url": "/url-shortener-is.gd/index.html"
   },
   {
-    "revision": "df83b9529024e38dcf69",
-    "url": "/url-shortener-is.gd/static/css/main.c07c7dbb.chunk.css"
+    "revision": "2ad18a197758c77544b7",
+    "url": "/url-shortener-is.gd/static/css/main.4b692972.chunk.css"
   },
   {
     "revision": "06c613465d82d8f54866",
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/url-shortener-is.gd/static/js/2.e798a9f2.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "df83b9529024e38dcf69",
-    "url": "/url-shortener-is.gd/static/js/main.dc3bd1c3.chunk.js"
+    "revision": "2ad18a197758c77544b7",
+    "url": "/url-shortener-is.gd/static/js/main.5513b359.chunk.js"
   },
   {
     "revision": "ebb7a032ee0a5b2fb483",
