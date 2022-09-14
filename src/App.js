d187e7d31a58a84bf9b6a85957b3dbde2a49a9ca
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-14 22:55:41 +0530

    :coffin: Removed pagination for temporary fix

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
-import React, { useEffect } from 'react';
-import { BrowserRouter, Switch, Route, Redirect } from 'react-router-dom';
-import localForage from 'localforage';
-import Navbar from './components/layout/Navbar';
-import Form from './components/layout/Form';
-import List from './components/layout/list/List';
-import Footer from './components/layout/Footer';
-import './scss/App.scss';
+import React, { useEffect } from "react";
+import { BrowserRouter, Switch, Route, Redirect } from "react-router-dom";
+import localForage from "localforage";
+import Navbar from "./components/layout/Navbar";
+import Form from "./components/layout/Form";
+import List from "./components/layout/list/List";
+import Footer from "./components/layout/Footer";
+import "./scss/App.scss";
 
 function App() {
   useEffect(() => {
-    if (localStorage.getItem('theme')) document.body.className = localStorage.getItem('theme');
+    if (localStorage.getItem("theme"))
+      document.body.className = localStorage.getItem("theme");
 
     localForage.config({
-      name: 'URL Shortener',
+      name: "URL Shortener",
     });
   }, []);
 
@@ -23,7 +24,8 @@ function App() {
         <div className="container">
           <Form />
           <Switch>
-            <Redirect exact from="/" to="/1" />
+            {/* <Redirect exact from="/" to="/1" /> */}
+            <Redirect exact from="/" />
             <Route exact from="/:page" component={List} />
           </Switch>
         </div>
