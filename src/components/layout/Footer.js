68d956dc132e7969e45f22fd82dcac494067b75f
AbdulSamad Ansari <hello@abdulsamad.dev>
2022-09-17 12:59:36 +0530

    :truck: Updated name in footer

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -5,7 +5,7 @@ function Footer() {
   return (
     <Foot className="footer z-depth-2" copyrights="&copy; 2020">
       <h5 className="white-text">
-        URL Shortner v{process.env.REACT_APP_VERSION}
+        MShortener v{process.env.REACT_APP_VERSION}
       </h5>
       <p className="grey-text text-lighten-4 valign-wrapper">
         Made with &nbsp; <Icon className="red-text">favorite</Icon>
